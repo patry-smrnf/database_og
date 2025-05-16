7109881d4fcd94f1f8b84e4200166281c26765e8
patryz-Z <patrykstepien05@gmail.com>
2025-05-15 18:42:29 -0700

    -Updated query

---
+++ cmake-build-debug/CMakeFiles/database_OG.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/patry/CLionProjects/database_og/Databases/database.cpp" "CMakeFiles/database_OG.dir/Databases/database.cpp.obj" "gcc" "CMakeFiles/database_OG.dir/Databases/database.cpp.obj.d"
+  "C:/Users/patry/CLionProjects/database_og/Databases/database_creator.cpp" "CMakeFiles/database_OG.dir/Databases/database_creator.cpp.obj" "gcc" "CMakeFiles/database_OG.dir/Databases/database_creator.cpp.obj.d"
+  "C:/Users/patry/CLionProjects/database_og/Databases/database_methods.cpp" "CMakeFiles/database_OG.dir/Databases/database_methods.cpp.obj" "gcc" "CMakeFiles/database_OG.dir/Databases/database_methods.cpp.obj.d"
+  "C:/Users/patry/CLionProjects/database_og/Databases/database_reader.cpp" "CMakeFiles/database_OG.dir/Databases/database_reader.cpp.obj" "gcc" "CMakeFiles/database_OG.dir/Databases/database_reader.cpp.obj.d"
+  "C:/Users/patry/CLionProjects/database_og/Misc/app.cpp" "CMakeFiles/database_OG.dir/Misc/app.cpp.obj" "gcc" "CMakeFiles/database_OG.dir/Misc/app.cpp.obj.d"
+  "C:/Users/patry/CLionProjects/database_og/main.cpp" "CMakeFiles/database_OG.dir/main.cpp.obj" "gcc" "CMakeFiles/database_OG.dir/main.cpp.obj.d"
+  "C:/Users/patry/CLionProjects/database_og/query/query.cpp" "CMakeFiles/database_OG.dir/query/query.cpp.obj" "gcc" "CMakeFiles/database_OG.dir/query/query.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/database_OG.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/database_OG.dir/Databases/database.cpp.obj"
+  "CMakeFiles/database_OG.dir/Databases/database.cpp.obj.d"
+  "CMakeFiles/database_OG.dir/Databases/database_creator.cpp.obj"
+  "CMakeFiles/database_OG.dir/Databases/database_creator.cpp.obj.d"
+  "CMakeFiles/database_OG.dir/Databases/database_methods.cpp.obj"
+  "CMakeFiles/database_OG.dir/Databases/database_methods.cpp.obj.d"
+  "CMakeFiles/database_OG.dir/Databases/database_reader.cpp.obj"
+  "CMakeFiles/database_OG.dir/Databases/database_reader.cpp.obj.d"
+  "CMakeFiles/database_OG.dir/Misc/app.cpp.obj"
+  "CMakeFiles/database_OG.dir/Misc/app.cpp.obj.d"
+  "CMakeFiles/database_OG.dir/main.cpp.obj"
+  "CMakeFiles/database_OG.dir/main.cpp.obj.d"
+  "CMakeFiles/database_OG.dir/query/query.cpp.obj"
+  "CMakeFiles/database_OG.dir/query/query.cpp.obj.d"
+  "database_OG.exe"
+  "database_OG.exe.manifest"
+  "database_OG.pdb"
+  "libdatabase_OG.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/database_OG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
